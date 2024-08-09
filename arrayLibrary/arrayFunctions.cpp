0a26f54194e58bfcb39188e313a97783d0b9cb94
Rishav Kumar <mrsonic622@gmail.com>
2024-08-10 00:54:21 +0530

    Update arrayFunctions.cpp

--- arrayLibrary/arrayFunctions.cpp
+++ arrayLibrary/arrayFunctions.cpp
@@ -14,9 +14,9 @@ void printArray(int arr[],int size){
     cout<<endl;
 }
 void alterSwap(int arr[],int size){
-for(int i=0;i<size;i+=2){//galti yaha pe thi bas...basics se hatoge to yhi hoga...
+for(int i=0;i<size;i+=2){
     if(i+1<size){
-      swap(arr[i],arr[i+1]);//yaha ye i=4 keliye access error aana chaiye tha because apna index hi 4 tak jayega 5 to hai hi nhi...but aaya nhi. why.
-    }//for safety   
+      swap(arr[i],arr[i+1]);
+    }   
 }
 }
