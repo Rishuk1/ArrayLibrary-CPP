d37903b8b0a3a9a402b7da0f5b7fc233bd428e3a
Rishav Kumar <mrsonic622@gmail.com>
2024-08-10 00:58:52 +0530

    Update arrayFunctions.h

--- arrayLibrary/arrayFunctions.h
+++ arrayLibrary/arrayFunctions.h
@@ -1,5 +1,4 @@
-// we still have to find out how we are going to make it work from another folder or in the same directory but with another folder...but for now enough playing with it...and what i can do is saare files ko dsadomination me daldo...thora confusing hojayega but fir bhi apan ik copy rakhenge iski......done
-// the command given below is what u need for the working of the files from main directory 
+// the command given below is what you need for the working of the files from main directory 
 // ..............base command...example
 // g++ randomCpp.cpp -IarrayLibrary -LarrayLibrary -lmyArrayLib -o myprogram
 #ifndef ARRAYFUNCTIONS_H
